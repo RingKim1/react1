983ac6c63751c8454a4b350f11631bd6b4cda5f6
RingKim1 <kgh9393@naver.com>
2024-05-14 15:52:19 +0900

    feat : add, remove기능  추가

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,19 @@ function App() {
   const [age, setAge] = useState("");
 
   const addUser = (e) => {
+    console.log("작동 여부 확인");
     e.preventDefault();
     // TODO: 이름과 나이가 모두 입력되지 않았을 때는 alert 처리하고 함수를 종료하세요. 논리합연산자 (||) 를 이용하세요.
-    if (!name || !age) { alert("이름과 나이를 모두 입력해주세요") }
+    if (!name || !age) { alert("이름과 나이를 모두 입력해주세요"); return;}
     // TODO: 사용자 리스트 상태를 업데이트 하세요. spread operator 를 사용하고, 추가되는 id는 현재 시간을 밀리초 단위로 반환하는 Date.now() 를 사용하세요.
+    setUsers([...users,{name:name, age:age, id:Date.now()}]);
+    setName("");
+    setAge("");
   };
 
   const removeUser = (id) => {
     // TODO: filter 메소드를 사용해서 사용자 삭제 로직을 구현해 보세요.
+    setUsers(users.filter((el)=>{return (el.id !== id);}));
   };
 
   return (
@@ -35,12 +40,15 @@ function App() {
       <ul>
         {/* TODO: map 메소드를 이용해서 user 리스트를 렌더링하세요.  */}
         {/* 이름: John, 나이: 20 [삭제] 버튼이 하나의 행에 나올 수 있도록 해보세요. (hint: flex) */}
-        {users.map((user) =>
-          <li>
-            <div>이름: {user.name}, 나이: {user.age}</div>
-            <button>삭제</button>
-          </li>
+        {users.map((user) => { return(
+          <li key={user.id} style={{display:"flex", justifyContent: "space-around"}}>
+            <div>이름: {user.name}</div>
+            <div>나이: {user.age}</div>
+            <button onClick={()=>{removeUser(user.id)}}>삭제</button>
+          </li>)
+          }
         )}
+
       </ul>
     </>
   );
